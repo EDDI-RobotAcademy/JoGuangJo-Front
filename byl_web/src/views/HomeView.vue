6c05331d995d1760e30b800ef2c5cc0c38298216
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-03-27 09:07:02 +0900

    remove:[박지영] HomeView 에 있던 임시 router-link 삭제 [BYL-FRONT-WEB-UI-15]
    메인페이지 UI가 구축되기 전에 임시로 HomeView 에 배치해 놓았던 router-link 들을 삭제합니다.

--- byl_web/src/views/HomeView.vue
+++ byl_web/src/views/HomeView.vue
@@ -1,22 +1,6 @@
 <template>
   <v-container>
- <!--<div>
-      <router-link to="/donate-choice">책 기부하기</router-link>
-     </div> 
-    
-    <nav>
-      <router-link to="/question-board-list-page">질문 게시판</router-link>
-    </nav> -->
-
-    <Main-Page-Form/>
-
-    <div>
-      <router-link to="/donate-choice">책 기부하기</router-link>
-    </div>
-    
-    <nav>
-      <router-link to="/question-board-list-page">질문 게시판</router-link>
-    </nav>
+    <main-page-form/>
   </v-container>
 </template>
 
