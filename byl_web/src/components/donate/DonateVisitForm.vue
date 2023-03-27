56243ae6b1d39945fefc4de7ff0b9505e1a3d91a
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-03-27 09:23:23 +0900

    add: [박지영] 방문수거페이지 입력폼 및 입력조건제한 [BYL-FRONT-WEB-UI-2]

--- byl_web/src/components/donate/DonateVisitForm.vue
+++ byl_web/src/components/donate/DonateVisitForm.vue
@@ -1,5 +1,41 @@
 <template>
-  <h3>방문 수거 페이지 입력폼</h3>
+    <div>
+      <v-form ref="form" v-model="valid" @submit.prevent="submit">
+          <v-row>
+            <v-col
+              cols="12"
+              sm="6"
+              md="6"
+            >
+
+            <v-text>
+              <h3>방문 수거 입력 폼</h3>
+            </v-text>
+
+            <v-text-field
+              label="이름"
+            ></v-text-field>
+
+            <v-text-field
+              label="이메일"
+            ></v-text-field>
+
+            <v-text-field
+              label="휴대전화번호"
+            ></v-text-field>
+
+              <v-menu
+                ref="visitDateMenu"
+                v-model="visitDateMenu"
+              >
+              </v-menu>
+
+              <v-menu
+                ref="visitTimeMenu"
+                v-model="visitTimeMenu"
+              >
+              </v-menu>
+
 </template>
 
 <script>
@@ -7,7 +43,18 @@ export default {
   name: 'DonateVisitForm'
 };
 </script>
+  name: "DonateVisitForm",
+  data() {
+    return {
+      },
+    };
+  },
 
-<style>
+  computed: {
+  },
 
-</style>
+  methods: {
+
+
+};
+</script>
