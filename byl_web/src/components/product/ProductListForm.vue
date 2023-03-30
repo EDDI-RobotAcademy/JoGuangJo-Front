000026b3ff634b5e5248e42e4ac3c95b99b55008
namm09155 <125858580+namm09155@users.noreply.github.com>
2023-03-30 15:47:05 +0900

    add: [남채율] 상품리스트페이지 카테고리 위치 변경, 페이징 추가 [BYL-FRONT-WEB-UI-64]

--- byl_web/src/components/product/ProductListForm.vue
+++ byl_web/src/components/product/ProductListForm.vue
@@ -1,6 +1,6 @@
 <template>
  <div class="product-list-form">
-  <div class="sticky-bar">
+ <!--  <div class="sticky-bar">
       <p>최근 본 상품:</p>
       <ul>
         <li v-for="(item, i) in recentItems" :key="i">
@@ -16,49 +16,58 @@
           <p>10시~16시</p>
         </li>
       </ul>
-    </div>
-    <v-container>
-
-    <v-container>
+    </div> -->
+    
+      <v-container>
       <v-row>
-        <v-col cols="12" sm="6">
-          <h2 class="title">상품 목록</h2>
+        <v-col cols="12" sm="3" class="category-container">
+          <h2 class="title">카테고리</h2>
+          <v-list dense>
+            <v-list-item-group color="primary">
+              <v-list-item v-for="(category, i) in categories" :key="i" @click="selectedCategory = category">
+                <v-list-item-content>
+                  <v-list-item-title>{{ category }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
         </v-col>
-        <v-col cols="12" sm="6" class="text-sm-right filter-container">
-          <v-select
-            v-model="selectedCategory"
-            :items="categories"
-            label="카테고리"
-            outlined
-            dense
-            class="filter-item"
-          ></v-select>
-          <v-select
-            v-model="sortBy"
-            :items="['이름순', '낮은 가격순', '높은 가격순']"
-            label="정렬"
-            outlined
-            dense
-            class="filter-item"
-          ></v-select>
+        <v-col cols="12" sm="9">
+          <v-row>
+            <v-col cols="12" sm="6">
+              <h2 class="title">상품 목록</h2>
+            </v-col>
+            <v-col cols="12" sm="6" class="text-sm-right filter-container">
+              <v-select
+               v-model="sortBy"
+               :items="['이름순', '낮은 가격순', '높은 가격순', '최신순']"
+               label="정렬"
+               outlined
+               dense
+               style="max-width: 150px;"
+               class="filter-item"
+              ></v-select>
+            </v-col>
+          </v-row>
+          <v-row>
+  <v-col v-for="(item, i) in paginatedProducts" :key="i" cols="12" sm="4" md="3" class="product-col">
+    <v-card class="elevation-6 product-card">
+      <v-img :src="item.img" :alt="`상품 이미지 ${i + 1}`" aspect-ratio="1" class="rounded"></v-img>
+      <v-card-text class="text-center">
+       <p class="product-name">{{ item.name }}</p>
+       <p class="product-price">{{ item.price }}원</p>
+       <p class="product-date">{{ item.date }}</p>
+       <p class="product-seller">판매자: {{ item.seller }}</p>
+      </v-card-text>
+      <v-card-actions class="justify-center">
+        <v-btn color="primary" text @click="addToCart(item)">장바구니에 담기</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</v-row>
+          <v-pagination v-model="currentPage" :length="totalPages" class="my-5"></v-pagination>
         </v-col>
       </v-row>
-
-      <v-row>
-        <v-col v-for="(item, i) in sortedProducts" :key="i" cols="12" sm="4" md="3" class="product-col">
-          <v-card class="elevation-6 product-card">
-            <v-img :src="item.img" :alt="`상품 이미지 ${i + 1}`" aspect-ratio="1" class="rounded"></v-img>
-            <v-card-text class="text-center">
-              <p class="product-name">{{ item.name }}</p>
-              <p class="product-price">{{ item.price }}원</p>
-            </v-card-text>
-            <v-card-actions class="justify-center">
-              <v-btn color="primary" text @click="addToCart(item)">장바구니에 담기</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
     </v-container>
   </div>
 </template>
@@ -72,18 +81,25 @@ export default {
       selectedCategory: "전체",
       categories: ["전체", "카테고리1", "카테고리2", "카테고리3"],
       recentItems: [
-        { img: "https://via.placeholder.com/200x200", name: "상품 1", price: "10,000" },
+        { img: "https://via.placeholder.com/200x200", name: "상품 1", price: "10,000" }, //스티키바
       ],
       products: [
-        { img: "https://via.placeholder.com/200x200", name: "상품 1", price: "10,000", category: "카테고리1" },
-        { img: "https://via.placeholder.com/200x200", name: "상품 2", price: "20,000", category: "카테고리2" },
+        { img: "https://via.placeholder.com/200x200", name: "상품 1", price: "10,000", category: "카테고리1", date: "2022-03-29", seller: "A"}, 
+        { img: "https://via.placeholder.com/200x200", name: "상품 2", price: "20,000", category: "카테고리2", date: "2023-03-29", seller: "B"},
         { img: "https://via.placeholder.com/200x200", name: "상품 3", price: "30,000", category: "카테고리3" },
         { img: "https://via.placeholder.com/200x200", name: "상품 4", price: "40,000", category: "카테고리1" },
         { img: "https://via.placeholder.com/200x200", name: "상품 5", price: "50,000", category: "카테고리2" },
         { img: "https://via.placeholder.com/200x200", name: "상품 1.3", price: "18,000", category: "카테고리1" },
         { img: "https://via.placeholder.com/200x200", name: "상품 2.7", price: "25,000", category: "카테고리2" },
         { img: "https://via.placeholder.com/200x200", name: "상품 4.6", price: "42,000", category: "카테고리3" },
+        { img: "https://via.placeholder.com/200x200", name: "상품 5.2", price: "62,000", category: "카테고리3" },
+        { img: "https://via.placeholder.com/200x200", name: "상품 6", price: "23,000", category: "카테고리2" },
+        { img: "https://via.placeholder.com/200x200", name: "상품 7", price: "72,000", category: "카테고리1" },
+        { img: "https://via.placeholder.com/200x200", name: "상품 8", price: "43,000", category: "카테고리1" },
+        { img: "https://via.placeholder.com/200x200", name: "넘어가라", price: "38,000", category: "카테고리2" },
       ],
+      currentPage: 1,
+      itemsPerPage: 12,
     };
   },
   computed: {
@@ -95,6 +111,8 @@ export default {
         sorted.sort((a, b) => parseInt(a.price) - parseInt(b.price));
       } else if (this.sortBy === "높은 가격순") {
         sorted.sort((a, b) => parseInt(b.price) - parseInt(a.price));
+      } else if (this.sortBy === "최신순") {
+        sorted.sort((a, b) => new Date(b.date) - new Date(a.date));
       }
 
       if (this.selectedCategory !== "전체") {
@@ -102,6 +120,14 @@ export default {
       }
 
       return sorted;
+    },
+  paginatedProducts() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.sortedProducts.slice(start, end);
+    },
+    totalPages() {
+      return Math.ceil(this.sortedProducts.length / this.itemsPerPage);
     },
   },
   methods: {
@@ -185,6 +211,7 @@ export default {
     font-size: 2rem;
     font-weight: bold;
     margin-bottom: 20px;
+    padding-left: 20px;
   }
   .product-name {
     font-size: 1.2rem;
@@ -216,5 +243,8 @@ export default {
   }
   .filter-item {
     margin-left: 10px;
+  }
+  .filter-item .v-input__control {
+    min-width: 80px;
   }
 </style>
