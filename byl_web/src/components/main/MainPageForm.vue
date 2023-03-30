7bacad9b03c02c05fe0b4d499b06b6d0b75a3a24
namm09155 <125858580+namm09155@users.noreply.github.com>
2023-03-30 15:45:12 +0900

    add: [남채율] 메인페이지 로고, 이미지크기 수정 [BYL-FRONT-WEB-UI-63]

--- byl_web/src/components/main/MainPageForm.vue
+++ byl_web/src/components/main/MainPageForm.vue
@@ -1,6 +1,6 @@
 <template> 
   <div class="main-page">
-
+  
     <v-carousel cycle hide-delimiters height="500" :interval="5000" class="carosel">
       <v-carousel-item v-for="(item, i) in items.slice(0, 5)" :key="i" :src="item.src"></v-carousel-item>
     </v-carousel>
@@ -11,7 +11,11 @@
     <v-col cols="12" sm="6" class="text-center"><br>
      <h2 class="title">책을 기부하는 이유</h2><br>
       <p class="description">
-       책 기부는 도서의 기부 및 새로운 도서를 제공하여 많은 이들이 책을 읽을 수 있는 기회를 제공합니다.
+       <!--책 기부는 도서의 기부 및 새로운 도서를 제공하여 많은 이들이 책을 읽을 수 있는 기회를 제공합니다.-->
+       책 기부는 지식과 경험을 나누는 소중한 기회입니다.<br>
+       필요한 사람들에게 책을 전달하여 많은 이들이 책을 읽을 수 있는 기회를 제공합니다.<br>
+       자신의 가치를 공유하고, 다양한 사람들과 연결될 수 있습니다. <br><br>
+       책을 기부하고, 소중한 가치를 나누어보세요.
       </p><br>
      <v-btn color="white" text-color="black" rounded href="/donate-choice" class="ml-0">책 기부하러 가기</v-btn>
     </v-col>
@@ -37,12 +41,12 @@
   </v-col>
 </v-row>
 
-<v-carousel cycle height="300" :interval="5000" hide-delimiters>
+<v-carousel cycle height="600" width="400" :interval="5000" hide-delimiters>
   <v-carousel-item v-for="n in Math.ceil(goodsItems.length / 3)" :key="n">
     <v-row justify="center">
       <v-col cols="4" v-for="(item, i) in goodsItems.slice((n - 1) * 3, n * 3)" :key="i">
         <v-card>
-          <v-img :src="item.img" :alt="`굿즈 이미지 ${i + 1}`" height="200"></v-img>
+          <v-img :src="item.img" :alt="`굿즈 이미지 ${i + 1}`" width="400" height="400"></v-img>
           <v-card-text class="text-center">
             <p class="goods-name">{{ item.name }}</p>
           </v-card-text>
@@ -60,12 +64,12 @@
     <v-btn color="white" text-color="black" rounded href="#">더보기</v-btn>
   </v-col>
 </v-row>
-<v-carousel cycle height="300" :interval="5000" hide-delimiters>
+<v-carousel cycle height="300" width="400" :interval="5000" hide-delimiters> <!--이미지 수정 다시-->
   <v-carousel-item v-for="(item, i) in bookItems" :key="i">
     <v-row justify="center">
       <v-col cols="4" v-for="(item, i) in bookItems.slice(i, i + 3)" :key="i">
         <v-card>
-          <v-img :src="item.img" :alt="`책 이미지 ${i + 1}`" height="200"></v-img>
+          <v-img :src="item.img" :alt="`책 이미지 ${i + 1}`" height="300"></v-img>
           <v-card-text class="text-center">
             <p class="book-name">{{ item.name }}</p>
           </v-card-text>
@@ -185,7 +189,7 @@ export default {
   padding-left: 0;
   padding-right: 0;
   margin-bottom: 30px;
-  margin-top: -10px;
+  margin-top: -68px;
 }
 .ml-0 {
   margin-right: -400px;
@@ -210,4 +214,5 @@ export default {
   font-size: 1.2rem;
   margin: 0;
 }
+
 </style>
