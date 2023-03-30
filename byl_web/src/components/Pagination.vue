062086f558f44c0852b88a6d8ddabd8f145ed23d
JinwooMa <125858213+JinwooMa@users.noreply.github.com>
2023-03-30 20:21:37 +0900

    feat: [마진우] 게시판 page 기능 components 분리[BYL-FRONT-WEB-UI-68]

---
+++ byl_web/src/components/Pagination.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="pagination">
+      <button @click="prevPage" :disabled="currentPage === 1">이전</button>
+      <span>{{ currentPage }} / {{ pageCount }}</span>
+      <button @click="nextPage" :disabled="currentPage === pageCount">다음</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Pagination",
+    props: {
+      currentPage: {
+        type: Number,
+        default: 1
+      },
+      pageCount: {
+        type: Number,
+        default: 1
+      }
+    },
+    methods: {
+      prevPage() {
+        if (this.currentPage > 1) {
+          this.currentPage--;
+          this.$emit('update:current-page', this.currentPage);
+        }
+      },
+      nextPage() {
+        if (this.currentPage < this.pageCount) {
+          this.currentPage++;
+          this.$emit('update:current-page', this.currentPage);
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .pagination {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+  }
+  
+  .pagination button {
+    background-color: #4caf50;
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 0 5px;
+    cursor: pointer;
+    border-radius: 5px;
+  }
+  
+  .pagination button:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
+  
+  .pagination span {
+    margin: 0 10px;
+    font-size: 16px;
+  }
+  </style>
+  
--- byl_web/src/components/boards/product/ProductListForm.vue
+++ byl_web/src/components/boards/product/ProductListForm.vue
@@ -25,8 +25,10 @@
           {{ index + (currentPage - 1) * pageSize + 1 }}
         </td>
         <td align="left">
-          <router-link :to="{ name: 'JpaProductReadPage',
-                            params: { productId: product.productId.toString() }}">
+          <router-link :to="{
+            name: 'JpaProductReadPage',
+            params: { productId: product.productId.toString() }
+          }">
             {{ product.productName }}
           </router-link>
         </td>
@@ -38,17 +40,18 @@
         </td>
       </tr>
     </table>
-    <div class="pagination">
-      <button :disabled="currentPage === 1" @click="currentPage -= 1">이전</button>
-      <span>{{ currentPage }} / {{ pageCount }}</span>
-      <button :disabled="currentPage === pageCount" @click="currentPage += 1">다음</button>
-    </div>
+    <Pagination :current-page="currentPage" :page-count="pageCount" @update:current-page="currentPage = $event" />
   </div>
 </template>
 
 <script>
+import Pagination from "@/components/Pagination.vue";
+
 export default {
   name: "JpaProductList",
+  components: {
+    Pagination
+  },
   props: {
     products: {
       type: Array
@@ -79,136 +82,110 @@ export default {
   },
   created() {
     //브라우저 새로고침 해도 상품 목록 계속 보이게 수정
-    if(this.products && this.products.length > 0) {
-    this.searchProducts();
+    if (this.products && this.products.length > 0) {
+      this.searchProducts();
     }
   },
   watch: {
     products() {
-    //브라우저 새로고침 해도 상품 목록 계속 보이게 수정
-    // 상품 목록이 변경될 때마다 searchProducts() 호출
-    if (this.products && this.products.length > 0) {
-      this.searchProducts();
+      //브라우저 새로고침 해도 상품 목록 계속 보이게 수정
+      // 상품 목록이 변경될 때마다 searchProducts() 호출
+      if (this.products && this.products.length > 0) {
+        this.searchProducts();
+      }
     }
-  }
   },
   methods: {
     searchProducts() {
-  const query = this.searchQuery.toLowerCase();
-  if (query === '') {
-    this.displayedProducts = this.products;
-  } else {
-    if (this.searchBy === 'productName') {
-      this.displayedProducts = this.products.filter(product => product.productName.toLowerCase().includes(query));
-    } else if (this.searchBy === 'seller') {
-      this.displayedProducts = this.products.filter(product => product.writer.toLowerCase().includes(query));
+      const query = this.searchQuery.toLowerCase();
+      if (query === '') {
+        this.displayedProducts = this.products;
+      } else {
+        if (this.searchBy === 'productName') {
+          this.displayedProducts = this.products.filter(product => product.productName.toLowerCase().includes(query));
+        } else if (this.searchBy === 'seller') {
+          this.displayedProducts = this.products.filter(product => product.writer.toLowerCase().includes(query));
+        }
+      }
+      this.currentPage = 1;
     }
-  }
-  this.currentPage = 1;
-}
-
   }
 };
 </script>
+<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+  margin-bottom: 20px;
+}
 
+th,
+td {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
 
-  
-  <style>
-  table {
-    border-collapse: collapse;
-    width: 100%;
-    margin-bottom: 20px;
-  }
-  
-  th, td {
-    border: 1px solid #ddd;
-    padding: 8px;
-  }
-  
-  th {
-    background-color: #f2f2f2;
-    color: #333;
-  }
-  
-  td {
-    vertical-align: middle;
-  }
-  
-  tr:nth-child(even) {
-    background-color: #f9f9f9;
-  }
-  
-  h3 {
-    font-size: 24px;
-    margin-bottom: 20px;
-  }
-  
-  .router-link-active {
-    font-weight: bold;
-  }
-  .pagination {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 20px;
-  }
-  .pagination button {
-    background-color: #f2f2f2;
-    color: #333;
-    border: none;
-    padding: 8px 16px;
-    margin: 0 4px;
-    cursor: pointer;
-    font-size: 16px;
-    border-radius: 4px;
-  }
-  .pagination button:hover {
-    background-color: #333;
-    color: #f2f2f2;
-  }
-  .pagination button:disabled {
-    opacity: 0.5;
-    cursor: default;
-  }
-  .search input[type="text"] { 
-      padding: 10px;
-      border-radius: 5px;
-      border: 1px solid #ccc;
-      width: 200px;
-      margin-right: 10px;
-      font-size: 16px;
-      background-repeat: no-repeat;
-      background-position: 5px center;
-      padding-left: 35px;
-  }
-  .search button {
-      background-color: #4CAF50;
-      border: none;
-      color: white;
-      padding: 10px 20px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      margin-top: 10px;
-      cursor: pointer;
-      border-radius: 5px;
-  }
-  .search select {
-    appearance: none;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    padding: 10px;
-    font-size: 16px;
-    margin-right: 10px;
-    cursor: pointer;
-  }
+th {
+  background-color: #f2f2f2;
+  color: #333;
+}
 
-  .search select:focus {
-    outline: none;
-    border-color: #4CAF50;
-  }
-  
-  
-  </style>
+td {
+  vertical-align: middle;
+}
+
+tr:nth-child(even) {
+  background-color: #f9f9f9;
+}
+
+h3 {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.router-link-active {
+  font-weight: bold;
+}
+
+.search input[type="text"] {
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  width: 200px;
+  margin-right: 10px;
+  font-size: 16px;
+  background-repeat: no-repeat;
+  background-position: 5px center;
+  padding-left: 35px;
+}
+
+.search button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.search select {
+  appearance: none;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 16px;
+  margin-right: 10px;
+  cursor: pointer;
+}
+
+.search select:focus {
+  outline: none;
+  border-color: #4CAF50;
+}
+</style>
