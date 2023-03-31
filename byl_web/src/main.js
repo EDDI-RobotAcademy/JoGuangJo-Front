bb79f646b464c0faa73a1835e9dacdb6d006e397
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-04-01 02:51:27 +0900

    init: [박지영] nomalize CSS 적용하기 [BYL-FRONT-WEB-UI-77]
    - 이제 모든 팀원은 cmd 창을 열고 byl_web 에 접속하여 `npm install` 명령어를 실행해 주세요.

--- byl_web/src/main.js
+++ byl_web/src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// vuetify 추가
 import vuetify from './plugins/vuetify'
 
+// bootstrap 추가
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// normalice.css 추가
+import 'normalize.css'
 
 Vue.use(BootstrapVue)
 
