2990eb0be32e7efb7d76e969ede7c58e38c63877
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2023-03-31 16:20:04 +0900

    feat: [안성희] axios로 Spring 과 연결 [BYL-FRONT-WEB-UI-69]

--- byl_web/src/views/mypage/MyPageView.vue
+++ byl_web/src/views/mypage/MyPageView.vue
@@ -16,12 +16,22 @@ export default {
         MyPageForm
     },
     mounted() {
-        // axios.post("http://localhost:7777/my-page/list")
-        //     .then(() => {
-        //         alert("http://localhost:7777/my-page/list 실행")
-        //         console.log("localStorage : " + localStorage.getItem("userInfo"))
-        //     })
-        
+      console.log("mounted 라이프 사이클 실행")
+
+      let userInfo = localStorage.getItem("userInfo")
+      console.log("초기 userInfo 데이터" + userInfo);
+      
+      let token =  userInfo.split(",")[0].split(":")[1];
+      console.log("token 값 : " + token)
+
+      let realtoken = token.substr(1, token.length - 2)
+      console.log(realtoken)
+      
+      axios.post("http://localhost:7777/mypage/list", realtoken)
+            .then((res) => {
+                alert(res.data)
+            })
+
     }
 }
 
