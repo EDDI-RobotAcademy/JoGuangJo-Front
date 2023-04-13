4b44f669b6a2fa82d541237beeea3029e2acfb8b
JinwooMa <125858213+JinwooMa@users.noreply.github.com>
2023-04-13 15:54:04 +0900

    feat: [마진우] 카카오페이 결제 기능 추가 [BYL-FRONT-WEB-UI-110]

---
+++ src/components/boards/product/ProductOrderForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2></h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductOrderForm',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
