385701ce21989d00524b727a8f1f82374fc69346
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-04-18 05:42:04 +0900

    refactor: [박지영] 기존 store 폴더의 파일들에 있던 donate 관련 부분 삭제 [BYL-FRONT-WEB-UI-118]
    store 폴더 안에 donate 폴더를 만들어서
    해당 도메인의 store 모듈화를 끝냈으므로
    
    기존의 store 폴더 들어가자마자 보이던
    actions.js, mutation-types.js, mutations.js, states.js 파일에서 donate 도메인과 관련된 부분들을 모두 지웠습니다.

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 
-import accountModule from '@/store/account/accountModule.js';
-
 import state from "@/store/states"
 import actions from "@/store/actions"
 import mutations from "@/store/mutations"
