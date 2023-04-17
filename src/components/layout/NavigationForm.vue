59458131f9a358fc421ac287bfb5a4cab2a2858b
namm09155 <125858580+namm09155@users.noreply.github.com>
2023-04-17 19:41:26 +0900

    style: [남채율] Navigation 장바구니 삭제[BYL-FRONT-WEB-UI-129]

--- src/components/layout/NavigationForm.vue
+++ src/components/layout/NavigationForm.vue
@@ -51,14 +51,14 @@
             <v-icon right>mdi-exit-to-app</v-icon>
             
             </v-btn>
-                        <v-btn
+                   <!--     <v-btn
             v-if="isAuthenticated == true"
             onclick="location.href='http://localhost:8080/cart-list-view'"
             class="btn"
             >
             장바구니
             <v-icon right></v-icon>
-            </v-btn>
+            </v-btn> 일단 삭제 -->
 
         </v-app-bar>
         
