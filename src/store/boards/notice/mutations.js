41b4edbb9b6fd8da608a90b8bc5eba29be543215
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-04-18 17:00:56 +0900

    refactor: [박지영} notice 도메인의 store 모듈화 및 기존 store 폴더에 있던 관련 부분 주석 처리 [BYL-FRONT-WEB-UI-59]
    - notice 도메인의 actions, states, mutation-types, mutations, noticeModule 부분에 필요한 코드 추가함
    (원래 모듈화 부분이랑 기존store 주석처리 부분을 한번에 올리려다가 보기 안좋아서 나눠서 올림)

---
+++ src/store/boards/notice/mutations.js
@@ -0,0 +1,20 @@
+import {
+
+    // 태현씨 공지게시판 -> 지영씨
+    REQUEST_NOTICE_BOARD_LIST_TO_SPRING,
+    REQUEST_NOTICE_BOARD_TO_SPRING
+    
+} from './mutation-types'
+
+
+export default {
+
+    // 태현씨 공지게시판 -> 지영씨
+    [REQUEST_NOTICE_BOARD_LIST_TO_SPRING] (state, passingData) {
+        state.noticeBoards = passingData
+    },
+    [REQUEST_NOTICE_BOARD_TO_SPRING] (state, passingData) {
+        state.noticeBoard = passingData
+    }
+    
+}
