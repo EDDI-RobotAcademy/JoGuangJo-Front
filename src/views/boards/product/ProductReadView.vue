c309727c91d499c5593614b9b8d00c55428532ea
JinwooMa <125858213+JinwooMa@users.noreply.github.com>
2023-04-19 15:37:46 +0900

    fix [마진우] 상품 상세보기 페이지 ui 수정 및 데이터 가지고 오지 못하는 오류 수정 [BYL-FRONT-WEB-UI-143]

--- src/views/boards/product/ProductReadView.vue
+++ src/views/boards/product/ProductReadView.vue
@@ -1,26 +1,28 @@
 <template>
   <v-container>
     <div align="center">
-      <h2 class="h2" style="margin-top:20px;">게시물 보기</h2>
+      <h2 class="h2">게시물 보기</h2>
       <product-read-form v-if="product" :product="product" :productImages="productImages" />
       <p v-else>로딩중 .......... </p>
-      <v-btn class="btn" color="primary" :to="{ name: 'ProductModifyView', params: { productId } }" v-if="isCurrentUserAuthor">
-        수정
-      </v-btn>
-      <v-btn class="btn" color="error" dark @click="onDelete" v-if="isCurrentUserAuthor">
-        삭제
-      </v-btn>
-      <v-btn class="btn" color="success" :to="{ name: 'ProductOrderView', params: { productId } }" v-if="isAuthenticated && !isCurrentUserAuthor">
-        구매하기
-      </v-btn>
-      <v-btn class="btn" :to="{ name: 'ProductListView' }">
-        돌아가기
-      </v-btn>
+      <div class="btn-container">
+        <v-btn class="back-btn" color="#fee789" rounded :to="{ name: 'ProductListView' }">
+          돌아가기
+        </v-btn>
+        <div>
+          <v-btn class="modify-btn" color="#fee789" rounded :to="{ name: 'ProductModifyView', params: { productId } }"
+            v-if="isCurrentUserAuthor">
+            수정
+          </v-btn>
+          <v-btn class="delete-btn" color="#fe6133" rounded dark @click="onDelete" v-if="isCurrentUserAuthor">
+            삭제
+          </v-btn>
+        </div>
+      </div>
     </div>
   </v-container>
 </template>
 
-  
+
 <script>
 
 import ProductReadForm from '@/components/boards/product/ProductReadForm.vue'
@@ -38,19 +40,19 @@ export default {
     }
   },
   computed: {
-  ...mapState(productModule, ['product', 'productImages']),
-  ...mapState("account", ["isAuthenticated"]),
-  isAuthenticated() {
-    return !!localStorage.getItem('userInfo');
+    ...mapState(productModule, ['product', 'productImages']),
+    ...mapState("account", ["isAuthenticated"]),
+    isAuthenticated() {
+      return !!localStorage.getItem('userInfo');
+    },
+    isCurrentUserAuthor() {
+      if (!this.isAuthenticated) {
+        return false;
+      }
+      const currentUser = JSON.parse(localStorage.getItem('userInfo')).nickName;
+      return this.product.writer === currentUser;
+    },
   },
-  isCurrentUserAuthor() {
-    if (!this.isAuthenticated) {
-      return false;
-    }
-    const currentUser = JSON.parse(localStorage.getItem('userInfo')).nickName;
-    return this.product.writer === currentUser;
-  },
-},
   methods: {
     ...mapActions(productModule, [
       'requestProductToSpring',
@@ -70,10 +72,28 @@ export default {
 }
 
 </script>
-  
+
 <style scoped>
 .h2 {
-  margin-bottom : -530px;
+  margin-bottom: 20px;
+  background-color: #fee789;
 }
 
+.btn-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+.back-btn {
+  margin-right: auto;
+}
+
+.modify-btn {
+  margin-right: 5px;
+}
+
+.delete-btn {
+  margin-left: 5px;
+}
 </style>
