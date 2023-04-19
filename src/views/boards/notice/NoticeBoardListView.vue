16ac4d09c0d1a3606f2ddd4259e884bf6f30f806
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-04-19 19:29:31 +0900

    feat: [박지영] notice 게시판에서 사용자의 role 상태에 따라 등록, 수정, 삭제 권한을 제한하는 기능 [BYL-FRONT-WEB-UI-59]

--- src/views/boards/notice/NoticeBoardListView.vue
+++ src/views/boards/notice/NoticeBoardListView.vue
@@ -1,9 +1,13 @@
 <template>
   <v-container>
     <!-- <h2>공지 게시판</h2> -->
-    <router-link :to="{ name: 'NoticeBoardRegisterView' }">
-      게시물 작성
-    </router-link>
+    <v-btn
+      v-if="canRegister"
+      router-link :to="{ name: 'NoticeBoardRegisterView' }"
+    >
+      게시물 등록하기
+    </v-btn>
+    
     <notice-board-list-form :noticeBoards="noticeBoards"/>
   </v-container>
 </template>
@@ -19,6 +23,13 @@ export default {
   name: "NoticeBoardListView",
   computed: {
     ...mapState( noticeModule, [ 'noticeBoards' ]),
+
+    // (지영) 공지게시판에서 사용자의 memberType에 따라 등록,수정,삭제 버튼이 보이거나 안 보이게 제한하는 메서드입니다.
+    // 공지게시판에서는 관리자만 등록 권한을 가집니다 (관리자만 등록버튼이 보임)
+    canRegister() {
+    const memberType = JSON.parse(localStorage.getItem('userInfo')).memberType;
+    return memberType === "Manager";
+    }
   },
   mounted () {
     this.requestNoticeBoardListToSpring()
