07b2c155877d89067b084c03cd3f9677ed9a3c6c
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2023-04-24 04:29:41 +0900

    update: [안성희] "마이페이지의 회원탈퇴 버튼 숨기기”[BYL-FRONT-WEB-UI-162]

--- src/components/layout/FooterForm.vue
+++ src/components/layout/FooterForm.vue
@@ -4,13 +4,14 @@
             <v-divider></v-divider>
             <div align="center">
                 <v-btn v-for="icon in icons" :key="icon" :to="getLink(icon)" class="mx-1 icon-btn" icon>
-                        <v-icon size="24px">
-                            {{ icon }} 
-                        </v-icon>
+                    <v-icon size="24px">
+                        {{ icon }} 
+                    </v-icon>
                 </v-btn>
             </div>
             <v-card-text class="pt-4">
-                서울특별시 강남구 테헤란로14길 6 남도빌딩 KH정보교육원<br><br>
+                서울특별시 강남구 테헤란로14길 6 남도빌딩 KH정보교육원<br>
+                <a v-if="showButton" style="color: grey;" @click="resignhandler" class="custom-link">회원 탈퇴</a><br>
                 <strong>ⓒ BOOK_YOU_LOVE</strong>
                 {{ new Date().getFullYear() }}
             </v-card-text>
@@ -33,19 +34,40 @@ export default {
         "mdi-facebook": "/facebook"
         }
     }),
+    computed: {
+        showButton() {
+            return this.$route.path === '/mypage';
+        }
+    },
     methods: {
         getLink(icon) {
             return this.links[icon];
-        }
+        },
+        async resignhandler() {
+            const result = await this.$store.dispatch("account/resign");
+            if (result) {
+                if (this.$route.path !== "/") {
+                    this.$router.push("/");
+                }
+            } else {
+                alert("회원탈퇴중에 오류가 발생했습니다.")
+            }
+        },
     }
 }
 </script>
 
 <style>
+    a.custom-link {
+        text-decoration: none;
+    }
 
-.icon-btn:hover {
-    background-color: rgba(255, 242, 189, 0.5);
-    color: #FFC107 !important;
-}
+    .hidden {
+        visibility: hidden;
+    }
 
+    .icon-btn:hover {
+        background-color: rgba(255, 242, 189, 0.5);
+        color: #FFC107 !important;
+    }
 </style>
