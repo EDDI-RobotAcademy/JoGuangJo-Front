648586aa04696c9c068ba58ce410c2423d7bad13
parkjiyoung <125860098+fdhjwail4865@users.noreply.github.com>
2023-04-23 20:14:15 +0900

    fix: [박지영] notice 게시판에서 사용자의 role 상태에 따라 등록, 수정, 삭제 권한을 제한하는 기능 [BYL-FRONT-WEB-UI-59]
    비로그인 상태에서 List, Read 페이지를 들어가면 아예 화면이 하얗게 날아가던 오류 수정
    (기존에 관리자만 버튼 보이게 하는 메서드가 비로그인 상태일 경우를 포함하지 않았기 때문이었음 — 현재는 고쳐서 비로그인 상태 페이지 들어갔을 때도 잘 동작합니다)

--- src/views/boards/notice/NoticeBoardReadView.vue
+++ src/views/boards/notice/NoticeBoardReadView.vue
@@ -58,10 +58,16 @@ export default {
 
     // (지영) 공지게시판에서 사용자의 memberType에 따라 등록,수정,삭제 버튼이 보이거나 안 보이게 제한하는 부분입니다.
     // 공지게시판에서는 관리자만 수정, 삭제 권한을 가집니다 (관리자만 수정, 삭제 버튼이 보임)
-    isManager() {
+      isManager() {
+        const userInfo = localStorage.getItem('userInfo');
+        
+        if (!userInfo) {
+          return false;
+        }
+        
         const memberType = JSON.parse(localStorage.getItem('userInfo')).memberType;
         return memberType === "Manager";
-        },
+      }
     },
     methods: {
         ...mapActions(noticeModule, [
